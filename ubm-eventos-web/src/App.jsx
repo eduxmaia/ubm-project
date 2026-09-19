3ad10d3c3ef65f2b186bb833462e6c095ed68295
Eduardo Caetano <educaetmaia3@gmail.com>
2026-09-19 18:29:51 -0300

    feat: campo de busca controlado com useState

--- ubm-eventos-web/src/App.jsx
+++ ubm-eventos-web/src/App.jsx
@@ -1,30 +1,43 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import EventoCard from "./components/EventoCard";
 import { eventos } from "./data/eventos";
 import "./App.css";
 import Rodape from "./components/Rodape";
 function App() {
- return (
- <>
- <Header />
- <main className="lista-eventos">
- {eventos.map((evento) => (
- <EventoCard
- key={evento.id}
- titulo={evento.titulo}
- tipo={evento.tipo}
- data={evento.data}
- local={evento.local}
- vagas={evento.vagas}
-  palestrante={evento.palestrante}
+  const [busca, setBusca] = useState('');
+  return (
+    <>
+      <Header />
+
+      <section className="busca">
+        <input
+          type="text"
+          placeholder="Buscar evento pelo título..."
+          value={busca}
+          onChange={(e) => setBusca(e.target.value)}
+        />
+        
+      </section>
+
+      <main className="lista-eventos">
+        {eventos.map((evento) => (
+          <EventoCard
+            key={evento.id}
+            titulo={evento.titulo}
+            tipo={evento.tipo}
+            data={evento.data}
+            local={evento.local}
+            vagas={evento.vagas}
+            palestrante={evento.palestrante}
 
 
-  
- />
- ))}
- </main>
- <Rodape />
- </>
- );
+
+          />
+        ))}
+      </main>
+      <Rodape />
+    </>
+  );
 }
 export default App;
