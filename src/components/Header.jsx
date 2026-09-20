5ab2bd488f41755428c70b1e781ee25345523107
Eduardo Caetano <educaetmaia3@gmail.com>
2026-09-20 17:41:52 -0300

    refactor: reorganiza estrutura do projeto

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <h1>UBM Eventos</h1>
+      <p className="app-header__aluno">Portal de Eventos Academicos - Por Eduardo Caetano</p>
+    </header>
+  )
+}
+
+export default Header
